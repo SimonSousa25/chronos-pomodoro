d43d7f9d988a109bbed137c1b8751773115253fa
SimonSousa25 <simonoliveirasousa02@email.com>
2025-12-09 19:23:30 -0300

    Organizando componentes em pastas padronizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { Heading } from './components/heading';
+import { Container } from './components/container';
 
 export function App() {
   return (
