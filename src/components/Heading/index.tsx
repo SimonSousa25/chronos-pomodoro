3c5212dd9b513f6f6f460be6834376f1b639ff66
SimonSousa25 <simonoliveirasousa02@email.com>
2025-12-17 01:16:35 -0300

    chore: resolvendo detecção de case no git

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  /** const classes = `${styles.heading} ${styles.cyan}`;
+   * dá pra desestruturar classes css dessa forma tbm, ou utilizar tudo dentro de className */
+  return <h1 className={styles.heading}>{children}</h1>;
+}
