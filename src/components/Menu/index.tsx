3d2facfe1d3222c949755ebe355816ebdaf2a059
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-02-08 13:09:10 -0300

    React Router - Iniciando o roteamento da aplicação

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import {
   SettingsIcon,
   SunIcon,
 } from 'lucide-react';
+import { Link } from 'react-router';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -40,14 +41,14 @@ export function Menu() {
 
   return (
     <nav className={styles.menu}>
-      <a
+      <Link
         className={styles.menuLink}
-        href='#'
+        to='/'
         aria-label='Ir para a Home'
         title='Ir para a Home'
       >
         <HouseIcon />
-      </a>
+      </Link>
 
       <a
         className={styles.menuLink}
