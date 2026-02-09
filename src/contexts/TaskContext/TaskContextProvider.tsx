c5f3c45f92a0de3f0911a12ddef3b531446dc5e6
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-02-09 12:15:19 -0300

    Atualizando o título da página a cada segundo da tarefa ativa (document.title)

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -40,6 +40,9 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     if (!state.activeTask) {
       worker.terminate();
     }
+
+    document.title = `${state.formattedSecondsRemaining} - Chronos Pomodoro`;
+
     worker.postMessage(state);
   }, [worker, state]);
 
