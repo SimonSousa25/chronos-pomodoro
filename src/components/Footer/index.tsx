58c3319132994e2afc2cffdc4f24cdeea4dc274c
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-02-09 12:04:31 -0300

    RouterLink - Criando meu próprio Link para o React Router

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router';
+import { RouterLink } from '../RouterLink';
+
 import styles from './styles.module.css';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link to='/about-pomodoro/'>
+      <RouterLink href='/about-pomodoro/'>
         Entenda como funciona a técnica pomodoro
-      </Link>
-      <Link to='/'>
+      </RouterLink>
+      <RouterLink href='/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💙
-      </Link>
+      </RouterLink>
     </footer>
   );
 }
